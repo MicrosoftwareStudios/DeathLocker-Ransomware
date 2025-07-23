36be74937e983e74be1243ad0c8a1ea096c0d157
Microsoftware <112523454+MicrosoftwareStudios@users.noreply.github.com>
2025-07-23 14:32:14 -0400

    Update win32.EXE.Ransom.DeathLocker Removal Tool.cmd

--- win32.EXE.Ransom.DeathLocker Removal Tool.cmd
+++ win32.EXE.Ransom.DeathLocker Removal Tool.cmd
@@ -1,9 +1,9 @@
 @echo off
-echo +------------------------------+
+echo "+------------------------------+"
 echo "| EXE-WIN32:Ransom.DeathLocker |"
 echo "|         Removal Tool         |"
 echo "| (c) Microsoftware Corp. 2025 |"
-echo +------------------------------+
+echo "+------------------------------+"
 echo.
 echo.
 echo NOTE: This program DOES NOT decrypt files, only removes malware files. Please delete the installer that installed DeathLocker.
